33ae259318245500e3afcee3147a1227dae8e94f
James Neufeld <jamesdneufeld@gmail.com>
2024-10-16 16:00:13 -0700

    hero change font animation

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,24 @@
-$(document).ready(function() {
-  $('.experiment-image-1').click(function() {
-    $('.experiment-image-2').fadeToggle("slow");
+$(document).ready(function () {
+  $(".experiment-image-1").click(function () {
+    $(".experiment-image-2").fadeToggle("slow");
     // Alternative animation for example
     // slideToggle("fast");
   });
+});
+
+$(document).mousemove(function (event) {
+  windowWidth = $(window).width();
+  windowHeight = $(window).height();
+
+  mouseXpercentage = Math.round((event.pageX / windowWidth) * 100);
+  mouseYpercentage = Math.round((event.pageY / windowHeight) * 100);
+
+  $(".radial-gradient").css(
+    "background",
+    "radial-gradient(at " +
+      mouseXpercentage +
+      "% " +
+      mouseYpercentage +
+      "%, #c0d1b6, #ffffff)"
+  );
 });
